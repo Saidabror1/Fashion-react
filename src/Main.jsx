4dd0c758cb0950f611648f249ca98d5842caf9e7
Saidabror Jamoliddinov <104624473+Saidabror1@users.noreply.github.com>
2023-03-23 10:59:26 +0500

    Add files via upload

---
+++ src/Main.jsx
@@ -0,0 +1,237 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+    <div className="home">
+        <div className="nav">
+            <div className="textlar">
+                <p>Home</p>
+                <p>Shop</p>
+                <p>Product</p>
+                <p>Page</p>
+                <p>Blog</p>
+            </div>
+            <img src="./ebRaw.svg"/>
+            <div className="input">
+                <input type="text" placeholder="Search products..."/>
+                <div><img src="./search.svg"/></div>
+            </div>
+            <img src="./icons.svg"/>
+        </div>
+        <div className="section1">
+            <div className="textlar2">
+                <p>New Arrival!</p>
+                <h1>MAN <br/>COLLECTION</h1>
+                <p className="p2">There are many variations of passages of Lorem Ipsum available majority have suffered alteration</p>
+                <button>SHOP NOW</button>
+            </div>
+            <img src="./left.svg" className="left"/>
+            <img src="./right.svg" className="right"/>
+            <img src="./3tochka.svg" className="tochka"/>
+        </div>
+        <div className="section2">
+            <div className="div1">
+                <div className="textlar">
+                    <p>Summer Sale</p>
+                    <h1>New Man Collection</h1>
+                    <button>Find Collection</button>
+                </div>
+            </div>
+            <div className="div2">
+                <div className="textlar2">
+                    <p>30-60% Off On</p>
+                    <h1>Man Bags Collection</h1>
+                    <button>Find Collection</button>
+                </div>
+            </div>
+            <div className="div3">
+                <div className="textlar3">
+                    <p>70% Off On</p>
+                    <h1>Man Watch Collection</h1>
+                    <button>Find Collection</button>
+                </div>
+            </div>
+        </div>
+        <div className="section3">
+            <div className="texts">
+                <h1>Best Selling Products</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                    ut labore et dolore magna aliqua. </p>
+            </div>
+            <div className="cards">
+                <div className="card1">
+                    <img src="./img1.svg"/>
+                    <h1>Man Sweet Hoodie</h1>
+                    <p>$250.00</p>
+                </div>
+                <div className="card2">
+                    <img src="./img2.svg"/>
+                    <h1>Man Summer Sweet Hoodie</h1>
+                    <p>$150.00</p>
+                </div>
+                <div className="card3">
+                    <img src="./img3.svg"/>
+                    <h1>Man Sweet Hoodie</h1>
+                    <p>$150.00</p>
+                    <div><button><img src="./icon.svg"/>ADD TO BAG</button></div>
+                </div>
+            </div>
+            <img src="./left.svg" className="left"/>
+            <img src="./right.svg" className="right"/>
+        </div>
+        <div className="section4">
+            <div>
+                <div className="texts">
+                    <h1>Super discount for your first purchase</h1>
+                    <button>FREE10DELIVERY</button>
+                    <p>Use discount code in checkout!</p>
+                </div>
+            </div>
+        </div>
+        <div className="section5">
+            <div className="texts">
+                <h1>What Say Our Regular Customer</h1>
+                <p className="p11">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+                <p className="p22">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                <img src="./avatar.svg" className="avatar"/>
+                <p className="p33">Wade Warren</p>
+                <p className="p44">UI designer</p>
+                <img src="./3tochka.svg"/>
+            </div>
+            <img src="./left.svg" className="left"/>
+            <img src="./right.svg" className="right"/>
+        </div>
+        <div className="section6">
+            <div className="text">
+                <h1>SAVE UP <label>50%</label> OFF</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+                <button>SHOP NOW</button>
+            </div>
+        </div>
+        <div className="section7">
+            <div className="card1">
+                <img src="i1.svg"/>
+                <h1>Free Shipping</h1>
+                <p>Orders over $200</p>
+            </div>
+            <div className="card1">
+                <img src="./i2.svg"/>
+                <h1>Secure Payment</h1>
+                <p>100% secure payment</p>
+            </div>
+            <div className="card2">
+                <img src="./i3.svg"/>
+                <h1>Gift Certificate</h1>
+                <p>Buy now $500 to $1000</p>
+            </div>
+            <div className="card1">
+                <img src="./i4.svg"/>
+                <h1>24/7 Support</h1>
+                <p>Ready support</p>
+            </div>
+        </div>
+        <div className="section8">
+            <div className="text">
+                <h1>From the blog</h1>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+            </div>
+            <div className="card1">
+                <img src="./ph1.svg"/>
+                <div>
+                    <p className="p1">DESIGN JULY 11, 2021</p>
+                    <h1>2021’S Biggest Sneaker Is Getting Us So Pumped For Summer</h1>
+                    <p className="p2">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't.</p>
+                    <button>Read More</button>
+                </div>
+            </div>
+            <div className="card2">
+                <img src="./ph2.svg"/>
+                <div>
+                    <p>DESIGN JULY 11, 2021</p>
+                    <h1>2021’S Biggest Sneaker Is Getting Us So Pumped For Summer</h1>
+                </div>
+                <button>Read More</button>
+            </div>
+            <div className="card3">
+                <img src="./ph3.svg"/>
+                <div>
+                    <p>DESIGN JULY 11, 2021</p>
+                    <h1>2021’S Biggest Sneaker Is Getting Us So Pumped For Summer</h1>
+                </div>
+                <button>Read More</button>
+            </div>
+        </div>
+        <div className="section9">
+            <h1>Sign Up Now & Get 10% Off</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className="input">
+                <input type="text" placeholder="Enter your email address"/>
+                <button>Subscribe Now</button>
+            </div>
+        </div>
+        <div className="section10">
+            <div className="text">
+                <img src='./insta.svg'/>
+                <h1>Follow us in instagram @jamoliddinovv1</h1>
+            </div>
+            <div className="imgs">
+                <img src="./1.svg"/>
+                <img src="./2.svg"/>
+                <img src="./3.svg"/>
+                <img src="./4.svg"/>
+            </div>
+        </div>
+        <div className="section11">
+            <div className="text1">
+                <img src="./ebRaw.svg" className="img1"/>
+                <p className="p1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                <img src="./socmedia.svg" className="img2"/>
+                <p className="p2">Check our shop in Google Map</p>
+            </div>
+            <div className="text2">
+                <h1>Support Links</h1>
+                <p>Product Guides
+                    Terms & Conditions
+                    Delivery Policy
+                    Privacy Policy
+                    Feedback</p>
+            </div>
+            <div className="text2">
+                <h1>Quick Links</h1>
+                <p>Product Guides
+                    Terms & Conditions
+                    Delivery Policy
+                    Privacy Policy
+                    Feedback</p>
+            </div>
+            <div className="text2">
+                <h1>Company</h1>
+                <p>Product Guides
+                    Terms & Conditions
+                    Delivery Policy
+                    Privacy Policy
+                    Feedback</p>
+            </div>
+            <div className="text3">
+                <h1>Gallery</h1>
+                <img src="./g1.svg"/>
+                <img src="./g2.svg"/>
+                <img src="./g3.svg"/>
+                <img src="./g4.svg"/>
+                <img src="./g5.svg"/>
+                <img src="./g6.svg"/>
+            </div>
+        </div>
+        <div className="section12">
+            <div className="text">
+                <p>Copyright @2023. All Rights Reserved By Saidabror</p>
+                <img src="./pay.svg"/>
+            </div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default Main
